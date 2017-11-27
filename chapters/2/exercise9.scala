d0816a7aa2d68a6a5b607cfe883d2e97efa2f15c
Bowofola 🙍🏽‍♂️💻 <bowofola@gmail.com>
2017-11-27 00:24:42 -0500

    recursive solution for ch2ex9

--- chapters/2/exercise9.scala
+++ chapters/2/exercise9.scala
@@ -1,4 +1,6 @@
 // 9. Make the function of the preceding exercise a recursive function.
 def productOfLetters(inputString: String) : BigInt = {
-    inputString.foldLeft(1:BigInt)((accumulator, nextChar) => accumulator * nextChar)
+    if(inputString.length == 0) 1
+    else
+        inputString.head * productOfLetters(inputString.tail)
 }
